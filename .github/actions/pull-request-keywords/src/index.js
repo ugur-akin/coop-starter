abb3612e167836f87742497a8b349b95870e76a8
Ugur Akin <ugurakinsoftware@gmail.com>
2021-12-31 17:09:05 +0300

    Pull request keywords now scrapes the PR for issues as well as collect labels from both the PR payload and issues. It compiles a set of keywords that are associated with the PR.

--- .github/actions/pull-request-keywords/src/index.js
+++ .github/actions/pull-request-keywords/src/index.js
@@ -1,8 +1,9 @@
 import { JSDOM } from "jsdom";
 import * as core from '@actions/core';
-import * as github from "@actions/github";
+import * as github from '@actions/github';
 import fetch from 'node-fetch';
 
+const issueHtmlRe = /https:\/\/github.com\/(?<org>.+)\/(?<repo>.+)\/issues\/(?<num>\d+)/
 
 const fetchPullRequest = async (url) => {
     const fetchOptions = {
@@ -19,24 +20,53 @@ const fetchPullRequest = async (url) => {
     return await resp.text();
 }   
 
-try {
-    const url = core.getInput('URL');
-    // const contextURL = github.context.payload.pull_request?.html_url
-    console.log(`URL from input: ${url}`);
-    // console.log(`URL from context: ${contextURL}`);
+const fromHtmlUrl = async (url) => {
+    const match = issueHtmlRe.match(url);
+    const {org, repo, num} = match.groups;
 
-    const html = await fetchPullRequest(url);
+    const result = `/${org}/${repo}/issues/${num}`;
+    return result;
+}
+
+try {
+    const prHtmlUrl = github.context.payload?.pull_request.html_url;
+
+    if (!prHtmlUrl){
+        throw new Error(
+            `Pull request URL is missing from context. Please ensure the action
+            is triggered with a pull request event.`);
+    }
+
+    const prPayload = github.context.payload.pull_request;
+    const auth = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(auth);
+
+    const html = await fetchPullRequest(prHtmlUrl);
     const { document } = new JSDOM(html).window;
+    
     const issuesForm = document.querySelector(`form[aria-label="Link issues"]`);
     const anchors = issuesForm.querySelectorAll("a")
     const issueURLs = []
     for (let anchor of anchors){
         const issueURL = anchor.getAttribute("href");
-        // const issue
         issueURLs.push(issueURL);
     }
 
-    core.setOutput('issueURLs', issueURLs);
+    const labels = prPayload.labels.map((label) => label.name);
+    const issueRequests = issueURLs.forEach( (url) => octokit.request(`GET ${fromHtmlUrl(url)}`) );
+    const issues = await Promise.all(issueRequests);
+
+    const issueTitles = issues.map((issue) => issue.title)
+    const issueLabels = issues.map((issue) => issue.labels.name);
+
+    const keywords = new Set();
+    labels.forEach((label) => keywords.add(label));
+    issueTitles.forEach((title) => keywords.add(title));
+    issueLabels.forEach((label) => keywords.add(label));
+
+    core.setOutput('issues', issues);
+    core.setOutput('labels', labels);
+    core.setOutput('keywords', keywords);
 }
 catch (err) {
     core.setFailed(err);
