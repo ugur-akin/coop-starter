719f7cc73ea9f362049ae068906e0a03ca332464
Ugur Akin <ugurakinsoftware@gmail.com>
2022-01-04 16:48:02 +0300

    Implemented a draft reviewer action.

---
+++ .github/actions/review-common-problem/src/index.js
@@ -0,0 +1,58 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+/**
+ * TODO: Implement a design pattern for reviewers:
+ *      - Inheritence
+ *      - Dependency injection (e.g. octo/review requests ready)
+ *      - Decorator?
+ *      - Composite actions in the future?
+ */
+
+const run = async () => {
+  try {
+    const tag = core.getInput("for");
+    const owner = core.getInput("owner");
+    const repository = core.getInput("repository");
+    const pullNumber = core.getInput("pull_number");
+    const reviewId = core.getInput("review_id");
+    const auth = core.getInput("GITHUB_TOKEN");
+    const octokit = github.getOctokit(auth);
+
+    const pullInput = github.getInput("pull_payload");
+    let pullRequest = pullInput;
+    if (!pullRequest) {
+      const { data: pullPayload } = await octokit.rest.pulls.get({
+        owner: owner,
+        repo: repository,
+        pull_number: pullNumber,
+        mediaType: {
+          format: "diff",
+        },
+      });
+
+      pullRequest = pullPayload;
+    }
+
+    switch (tag) {
+      case "suboptimal-unedited-title":
+        const passed = SuboptimalTitleReviewer(pullRequest);
+        octokit.rest.pulls.reviews.post({
+          owner: owner,
+          repo: repository,
+          pull_number: pullNumber,
+          review_id: reviewId,
+          body: "Please use a descriptive title as close to plain english as possible.",
+        });
+        break;
+      default:
+        throw new Error(
+          `Aborted review process: Review tag ${tag} is incorrect or its automated review is not yet implemented.`
+        );
+    }
+  } catch (err) {
+    core.setFailed(err);
+  }
+};
+
+run();
---
+++ .github/actions/review-common-problem/src/reviewers/SuboptimalTitleReviewer.js
@@ -0,0 +1,23 @@
+// TODO: Implement a class/inheritance based design here?
+
+const wsReplaced = /-_/;
+
+const branchToTitle = (str) => {
+  const removeLines = str.replace(wsReplaced, " ");
+  const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+  return capitalized;
+};
+
+const SuboptimalTitleReviewer = (pull) => {
+  const {
+    title,
+    base: { label: baseBranch },
+    head: { label: headBranch },
+  } = pull;
+
+  const defaultTitle = branchToTitle(head);
+  const passed = defaultTitle !== title;
+
+  return passed;
+};
